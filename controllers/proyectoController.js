0673e78fb590448a272b3d8879a317494d344da0
valentin <valentinaless07@hotmail.com>
2022-03-26 01:15:31 -0300

    eliminarColaborador

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -25,7 +25,7 @@ const nuevoProyecto = async (req, res) => {
 
 const obtenerProyecto = async (req, res) => {
     const {id} = req.params
-    const proyecto = await Proyecto.findById(id).populate('tareas')
+    const proyecto = await Proyecto.findById(id).populate('tareas').populate('colaboradores', 'nombre email')
 
    if(!proyecto){
        const error = new Error("No encontrado")
@@ -144,6 +144,22 @@ const agregarColaborador = async (req, res) => {
 }
 
 const eliminarColaborador = async (req, res) => {
+    const proyecto = await Proyecto.findById(req.params.id)
+    if(!proyecto) {
+        const error = new Error("Proyecto No Encontrado")
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error("Acción no válida")
+        return res.status(401).json({msg: error.message})
+    }
+    
+
+
+    proyecto.colaboradores.pull(req.body.id)
+    await proyecto.save()
+    res.json({msg: 'Colaborador eliminado correctamente'})
     
 }
 
