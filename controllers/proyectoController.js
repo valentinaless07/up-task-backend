726fb968dd8e86de555f3db7e766b9bcb2ba6960
valentin <valentinaless07@hotmail.com>
2022-03-28 01:50:03 -0300

    previewColaboradores

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -3,7 +3,9 @@ import Usuario from "../models/Usuario.js"
 
 const obtenerProyectos = async (req, res) => {
 
-    const proyectos = await Proyecto.find().where('creador').equals(req.usuario).select('-tareas')
+    const proyectos = await Proyecto.find({
+        '$or': [{'colaboradores': {$in: req.usuario}}, {'creador': {$in: req.usuario}}]
+    }).select('-tareas')
 
     res.json(proyectos)
 
@@ -32,7 +34,7 @@ const obtenerProyecto = async (req, res) => {
        return res.status(404).json({msg: error.message})
    }
 
-   if(proyecto.creador.toString() !== req.usuario._id.toString() ){
+   if(proyecto.creador.toString() !== req.usuario._id.toString() && !proyecto.colaboradores.some(colaborador => colaborador._id.toString() === req.usuario._id.toString())){
         const error = new Error("Acción no válida")
        return res.status(401).json({msg: error.message})
    }
--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -87,7 +87,11 @@ const eliminarTarea = async (req, res) => {
     }
 
     try {
-        await tarea.deleteOne()
+        const proyecto = await Proyecto.findById(tarea.proyecto)
+        proyecto.tareas.pull(tarea._id)
+
+        await Promise.allSettled([await proyecto.save(), await tarea.deleteOne()])
+
         res.json({msg: "La tarea se eliminó"})
     } catch (error) {
         console.log(error)
@@ -95,7 +99,28 @@ const eliminarTarea = async (req, res) => {
 }
 
 const cambiarEstado = async (req, res) => {
-    
+    const {id} = req.params
+    const tarea = await Tarea.findById(id).populate('proyecto')
+
+    if(!tarea){
+        const error = new Error('Tarea no encontrada')
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(tarea.proyecto.creador.toString() !== req.usuario._id.toString() &&
+    !tarea.proyecto.colaboradores.some(colaborador => colaborador._id.toString() === req.usuario._id.toString())
+    ){
+        const error = new Error('Acción no válida')
+        return res.status(403).json({msg: error.message})
+    }
+
+    tarea.estado = !tarea.estado
+
+    await tarea.save()
+
+    res.json(tarea)
+
+
 }
 
 export {
